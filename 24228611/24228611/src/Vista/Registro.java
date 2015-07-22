aeed61e68ab0ca9ebbc68a8d4f1748c03f94a3fe
root <root@canaima-popular.unassigned>
2015-07-22 11:31:41 -0430

    Modificacion

--- 24228611/24228611/src/Vista/Registro.java
+++ 24228611/24228611/src/Vista/Registro.java
@@ -2,12 +2,23 @@ package Vista;
 
 import java.awt.BorderLayout;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import javax.swing.JButton;
 
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class Registro extends JFrame{
 
@@ -15,7 +26,16 @@ public class Registro extends JFrame{
 	private JDesktopPane ContenedorPrincipal = null;
 	private JInternalFrame IntFrame = null;
 	
+        private JMenuBar MenuPrincipal = null;
+        private JMenu archivo = null;
+        private JMenuItem nuevo = null;
+        
 	private JLabel inicio = null;
+        private JLabel usuario, clave = null;
+        private JTextField txtusuario = null;
+        private JPasswordField txtclave = null;
+        
+        private JButton btnaceptar, btnlimpiar, btncancelar = null;
 	
 	private static final long serialVersionUID = 1459206530077350977L;
 
@@ -25,11 +45,38 @@ public class Registro extends JFrame{
 	
 	public void Inicializar(){
 		this.setTitle("Registro");
-		this.setSize(300,300);
+		this.setSize(400,400);
 		this.setLocation(200,300);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setContentPane(getContenedorPrincipal());
-		
+		this.setJMenuBar(MenuContenedor());
+	}
+        
+        //Inicio del codigo para el menu
+	private JMenuBar MenuContenedor(){
+		if (MenuPrincipal == null){
+			MenuPrincipal = new JMenuBar();
+                        MenuPrincipal.setVisible(false);
+			MenuPrincipal.add(ArchivoMenu());
+		}
+		return MenuPrincipal;
+	}
+	
+	private JMenu ArchivoMenu(){
+		if (archivo == null){
+			archivo = new JMenu();
+			archivo.setText("Archivo");
+			archivo.add(NuevoArchivo());
+		}
+		return archivo;
+	}
+        
+        	private JMenuItem NuevoArchivo(){
+		if (nuevo == null){
+			nuevo = new JMenuItem();
+			nuevo.setText("Nuevo");
+		}
+		return nuevo;
 	}
 	
 	private JDesktopPane getContenedorPrincipal() {
@@ -44,7 +91,7 @@ public class Registro extends JFrame{
 		if (IntFrame == null){
 			IntFrame = new JInternalFrame();
 			IntFrame.setVisible(true);
-			IntFrame.setBounds(new Rectangle(50,20,200,200));
+			IntFrame.setBounds(new Rectangle(50,20,300,300));
 			IntFrame.setContentPane(ContPanel());
 			//Componentes
 			
@@ -55,7 +102,16 @@ public class Registro extends JFrame{
 	private JPanel ContPanel(){
 		if (panel == null){
 			panel = new JPanel();
-			panel.setLayout(new BorderLayout());
+			panel.setLayout(null);
+                        //Componentes
+                        panel.add(getInicio(), null);
+                        panel.add(getNombre(), null);
+                        panel.add(getTxtUsuario(), null);
+                        panel.add(getClave(), null);
+                        panel.add(getTxtClave(), null);
+                        panel.add(Aceptar(), null);
+                        panel.add(Limpiar(), null);
+                        panel.add(Cancelar(), null);
 		}
 		return panel;
 	}
@@ -64,9 +120,115 @@ public class Registro extends JFrame{
 		if (inicio == null){
 			inicio = new JLabel();
 			inicio.setText("Iniciar Sesion");
-			inicio.setBounds(new Rectangle(10,20,100,20));
+			inicio.setBounds(new Rectangle(20,20,100,20));
 		}
 		return inicio;
+	}
+        
+        	private JLabel getNombre(){
+		if (usuario == null){
+			usuario = new JLabel();
+			usuario.setText("Usuario");
+			usuario.setBounds(new Rectangle(10,50,100,20));
+		}
+		return usuario;
+	}
+                
+                	private JTextField getTxtUsuario(){
+		if (txtusuario == null){
+			txtusuario = new JTextField();
+			txtusuario.setBounds(new Rectangle(70,50,100,20));
+		}
+		return txtusuario;
+	}
+                        
+              private JLabel getClave(){
+		if (clave == null){
+			clave = new JLabel();
+			clave.setText("Clave");
+			clave.setBounds(new Rectangle(10,70,100,20));
+		}
+		return clave;
+	}
+                
+                	private JPasswordField getTxtClave(){
+		if (txtclave == null){
+			txtclave = new JPasswordField();
+			txtclave.setBounds(new Rectangle(70,70,100,20));
+		}
+		return txtclave;
+	}
+                        
+         	private JButton Aceptar(){
+		if (btnaceptar == null){
+			btnaceptar = new JButton();
+			btnaceptar.setText("Aceptar");
+                        btnaceptar.setBounds(new Rectangle(1,110,120,20));
+                        
+                         btnaceptar.addMouseListener(new MouseAdapter() {
+				   public void mouseClicked(java.awt.event.MouseEvent e){
+                                       
+                                       String a = txtusuario.getText();
+                                       String b =  txtclave.getText();
+                                       
+                                       String id = "admin";
+                                       String pass = "12345";
+                                       
+                                       if (a.equals("") && b.equals("")){
+                                           JOptionPane.showMessageDialog(null, "Ingrese los datos", "Mensaje de Advertencia", 0);
+                                       }
+                                       
+                                       else if (id.equals(txtusuario.getText()) && pass.equals(txtclave.getText())) {
+                                           JOptionPane.showMessageDialog(null, "Bienvenido", "Mensaje de Bienvenido", 1);
+                                           
+                                           MenuContenedor().setVisible(true);
+                                       }
+                                       else if (a.equals("")){
+                                           JOptionPane.showMessageDialog(null, "Ingrese Usuario", "Mensaje de Advertencia", 0);
+                                       }
+                                       else if (b.equals("")){
+                                           JOptionPane.showMessageDialog(null, "Ingrese Clave", "Mensaje de Advertencia", 0);
+                                       }
+                                        
+                                       else JOptionPane.showMessageDialog(null, "Usuario o Clave Incorrecto", "Mensaje de Error", 0);
+				   }
+			   });
+                        
+		}
+		return btnaceptar;
+	}
+	
+	private JButton Limpiar(){
+		if (btnlimpiar== null){
+			btnlimpiar = new JButton();
+			btnlimpiar.setText("Limpiar");
+			btnlimpiar.setBounds(new Rectangle(130,110,120,20));
+                        
+                        btnlimpiar.addMouseListener(new MouseAdapter() {
+				   public void mouseClicked(java.awt.event.MouseEvent e){
+					   
+                                       txtusuario.setText(null);
+                                       txtclave.setText(null);
+				   }
+			   });
+		}
+		return btnlimpiar;
+	}
+        
+        	private JButton Cancelar(){
+		if (btncancelar== null){
+			btncancelar = new JButton();
+			btncancelar.setText("Cancelar");
+			btncancelar.setBounds(new Rectangle(70,140,120,20));
+                        
+                         btncancelar.addMouseListener(new MouseAdapter() {
+				   public void mouseClicked(java.awt.event.MouseEvent e){
+					   
+                                       dispose();
+				   }
+			   });
+		}
+		return btncancelar;
 	}
 	
 	public static void main(String[] args) {
